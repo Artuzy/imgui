c99ca0562930eaa06b32b7a0218de4645be089d3
Artuzy <60360967+Artuzy@users.noreply.github.com>
2020-02-15 16:41:06 +0200

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,4 +1,4 @@
-project "GLFW"
+project "ImGui"
 	kind "StaticLib"
 	language "C"
 
@@ -7,40 +7,22 @@ project "GLFW"
 
 	files
 	{
-		"include/GLFW/glfw3.h",
-		"include/GLFW/glfw3native.h",
-		"src/glfw_config.h",
-		"src/context.c",
-		"src/init.c",
-		"src/input.c",
-		"src/monitor.c",
-		"src/vulkan.c",
-		"src/window.c"
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp"
 	}
 
 	filter "system:windows"		
 		systemversion "latest"
+        cppdialect "C++17"
 		staticruntime "On"
 
-		files
-		{
-			"src/win32_init.c",
-			"src/win32_joystick.c",
-			"src/win32_monitor.c",
-			"src/win32_time.c",
-			"src/win32_thread.c",
-			"src/win32_window.c",
-			"src/wgl_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c"
-		}
-
-		defines 
-		{ 
-			"_GLFW_WIN32",
-			"_CRT_SECURE_NO_WARNINGS"
-		}
-
 	filter { "system:windows", "configurations:Release" }
-	    buildoptions "/MT"
-		
+	    buildoptions "/MT"	
